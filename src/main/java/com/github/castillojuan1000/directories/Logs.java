bcbc1cdb8d13b951ded4ff859e3dd21956f69b39
Juan Castillo <castillojuan1000@gmail.com>
2023-03-01 08:22:38 -0500

    #9 #12 created USER_BUILDS paths and added dir global paths

--- src/main/java/com/github/castillojuan1000/directories/Logs.java
+++ src/main/java/com/github/castillojuan1000/directories/Logs.java
@@ -5,5 +5,7 @@ import java.io.File;
 public class Logs {
   public static void createLogsDir(String parentDirPath){
     new File(parentDirPath+"/logs").mkdirs();
+    String buildLogsPath = parentDirPath+"/logs";
+    System.setProperty("BUILD_LOGS_PATH", buildLogsPath);
   }
 }
